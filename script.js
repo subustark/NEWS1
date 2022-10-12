6bf106574268aab47ec3875a7dfcb424fcaae549
subustark <113083918+subustark@users.noreply.github.com>
2022-10-12 12:28:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function foo(){
+    var res=document.getElementById("name").value;
+    var res1=document.getElementById("name1").value;
+    var res2=document.getElementById("name2").value;
+    var res3=document.getElementById("email").value;
+    var res4=document.getElementById("pass").value;
+
+    console.log(res,res1,res2,res3,res4);
+}
